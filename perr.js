3fcb27f82fd04bd5eb3b1a952a3c64d2def9ab96
ashish1508 <belide.ashish@gmail.com>
2018-01-18 17:46:46 +0530

    Add files via upload

---
+++ perr.js
@@ -0,0 +1,24 @@
+var express = require('express')
+var bodyparser = require('body-parser');
+var app = express()
+app.use(bodyparser());
+app.set('view engine','ejs');
+// … Configure Express, and register necessary route handlers
+app.get('/chat/home',function(req,res){
+  res.render('perr');
+});
+app.get('/chat/:pid',function(req,res){
+	console.log('pid :'+req.params.pid)
+  res.render('perr2',{id : req.params.pid});
+});
+app.get('/chat/scall/:pid',function(req,res){
+  res.render('call',{id:req.params.pid});
+});
+app.get('/rcall',function(req,res){
+  res.render('receive');
+});
+srv = app.listen(3000);
+
+app.use('/peerjs', require('peer').ExpressPeerServer(srv, {
+	debug: true
+}))
